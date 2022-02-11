d806cd03adddb30e4cc90cbf04b23d7e2960f0e5
ysksd <shoh.7514@gmail.com>
2022-02-11 16:21:13 +0900

    削除機能と保存機能を追加

--- practice/ViewController.swift
+++ practice/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     
     var todoList = [String]()
+    let userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
+            todoList.append(contentsOf: storedTodoList)
+        }
     }
 
     @IBAction func addBtnAction(_ sender: Any) {
@@ -25,6 +29,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             if let textField = alertController.textFields?.first {
                 self.todoList.insert(textField.text!, at: 0)
                 self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
+                self.userDefaults.set(self.todoList, forKey: "todoList")
             }
         }
         alertController.addAction(okAction)
@@ -37,11 +42,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return todoList.count
     }
     
-    func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         let todoTitle = todoList[indexPath.row]
         cell.textLabel?.text = todoTitle
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            todoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+            self.userDefaults.set(self.todoList, forKey: "todoList")
+        }
     }
 }
 
